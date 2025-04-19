aa98bf2781f9da43e35f0136f25ea2781117727f
Floppathebig <1102ka@mail.ru>
2025-04-19 19:32:42 +0300

    maked A0-2

---
+++ Base_A/mainA1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+   printf("	 Let's\n" "           go\n" "             to walk\n");
+   return 0;
+
+
+
+
+}
---
+++ Base_A/mainA2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+   printf("     *\n" "    ***\n" "   *****\n" "  *******\n" "   HHOHH\n" "   ZZZZZ\n");
+   return 0;
+
+
+
+
+}
